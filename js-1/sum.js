68835dbee3dce36cda0f6327d5b18cf311599ba5
Andres <anglodos@gmail.com>
2023-03-20 02:16:48 -0500

    solucion a los ejercicios de js

---
+++ js-1/sum.js
@@ -0,0 +1,12 @@
+// escribe la función suma acá
+
+function sum(num) {
+    if (num <= 1) {
+        return num
+    }
+    return num + sum(num - 1)
+}
+// código de prueba
+console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
+console.log(sum(10)) // 1+2+3+4+5+6+7+8+9+10 = 55
+console.log(sum(15)) // 120 
