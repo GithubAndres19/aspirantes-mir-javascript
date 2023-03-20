a8bc7052d163b2fa96dc881885bad72c753c7ab7
Andres <anglodos@gmail.com>
2023-03-20 14:13:18 -0500

    actualizacion

--- js-1/IMC.js
+++ js-1/IMC.js
@@ -1,12 +1,9 @@
 // escribe la función bmi acá
 function bmi(peso, altura) {
-    if (peso <= 0) {
-        return peso + " el valor no puede ser menor a 1"
-        if (altura <= 0)
-            return altura + " el valor no puede ser menor a 1"
+
+    if (peso <=0 || altura <=0) return 0;
     }
     return peso / altura ** 2
-}
 // código de prueba
 console.log(bmi(65, 1.8)) // 20.061728395061728
 console.log(bmi(72, 1.6)) // 28.124999999999993
