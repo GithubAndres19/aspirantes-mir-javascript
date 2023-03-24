9da89bfb32444948f1bc12ce1b5378bb96e0b17d
Andres <anglodos@gmail.com>
2023-03-24 08:38:24 -0500

    se realiza actualizacion

--- js-3/object1.js
+++ js-3/object1.js
@@ -1,26 +1,16 @@
-
-//1 Crea una variable llamada pedro de tipo objeto literal 
 let pedro = {
-nombre: "Pedro Perez",
-edad: 30,
-activo: true,
-hobbies: ["programar", " squash", " piano"],
-//6 Agrégale al objeto una función llamada saluda que retorne la frase 
-//“Hola, me llamo ” seguido del nombre de la persona.
-saluda: function () { console.log ("Hola, me llamo " + pedro.nombre)} 
+    nombre: 'Pedro Perez',
+    edad: 30,
+    activo: true,
+    hobbies: ["programar", "squash", "piano"],
+};
+console.log(pedro['edad']);
+pedro.estatura = 1.8;
+delete pedro.activo;
+for (const key in pedro) {
+    console.log(key, ": ", pedro[key]);
 }
-//2 Imprime en la consola el valor de la llave edad.
-console.log (pedro.edad)
-//3 Agrega una propiedad con llave estatura y valor 1.8
-pedro.estatura = 1.8
-//4 Elimina la propiedad con llave activo.
-delete pedro.activo
-//5 Recorre todas las propiedades e imprímelas en consola 
-//(una línea por propiedad y separando la llave y el valor con dos puntos :).
-console.log("nombre: "+ pedro.nombre)// imprime nombre: Pedro Perez
-console.log("edad: "+ pedro.edad)// imprime edad: 30
-console.log("activo: "+ pedro.activo)// imprime activo: undefined
-console.log("hobbies: "+ pedro.hobbies)// imprime hobbies: programar, squash, piano
-console.log("estatura: "+ pedro.estatura)// imprime estatura: 1.8
-//7 Llama la función saluda y verifica el resultado, debería decir “Hola, me llamo Pedro Perez”.
-pedro.saluda()
+pedro.saluda = function () {
+    return `Hola, me llamo ${this.nombre}`;
+}
+console.log(pedro.saluda());
--- js-3/object2.js
+++ js-3/object2.js
@@ -1,25 +1,17 @@
-//1 Crea un objeto literal llamado "persona" 
-//con las siguientes propiedades: nombre, edad, ciudad y profesión.
-let persona= {
-    nombre:"Samuel Garcia",
+let persona = {
+    nombre: "Samuel Garcia",
     edad: 10,
-    ciudad:"Bogotá",
-    profesion: "Estudiante",}
-//2 Imprime en la consola el objeto "persona" completo.
-console.log (persona)
-    // 3.1 Crea una función llamada "presentacion" que tome el objeto "persona" como argumento 
-    // y devuelva una cadena de texto que incluya el nombre, la edad y la ciudad de la persona.
-    persona.presentacion = function () {
-        return `${this.nombre} ${this.edad} ${this.ciudad} ${this.profesion}`;}
+    ciudad: "Bogotá",
+    profesion: "Estudiante",
+}
+console.log(persona)
+persona.presentacion = function () {
+    return `${this.nombre} ${this.edad} ${this.ciudad} ${this.profesion}`;
+}
 console.log(persona.presentacion())
-//6 Agrega una nueva propiedad al objeto "persona" llamada "hobbies" que sea un arreglo 
-//con algunos pasatiempos.
 persona.hobbies = ["jugar", "leer", "ver videos"]
-//7 Imprime en la consola la propiedad "hobbies" del objeto "persona".
 console.log("\nHobbies:")
-//Utiliza un ciclo for para imprimir en la consola 
-//cada uno de los elementos del arreglo de hobbies.
-for (let i = 0; i < persona.hobbies.length; i++){
+for (let i = 0; i < persona.hobbies.length; i++) {
     console.log(persona.hobbies[i])
 }
 
